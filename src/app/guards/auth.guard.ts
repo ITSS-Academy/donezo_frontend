467db24055218f8498da92942373040554fa3351
Bodu <tn212079@gmail.com>
2025-02-27 13:42:00 +0700

    feat: fix guard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,22 +1,21 @@
 import {CanActivateFn, Router} from '@angular/router';
 import {inject} from '@angular/core';
-import {Store} from '@ngrx/store';
-import {AuthState} from '../ngrx/auth.state';
-import {first, map, tap} from 'rxjs';
+import {Auth, onAuthStateChanged} from '@angular/fire/auth';
 
 
 export const authGuard: CanActivateFn = (route, state) => {
   const router = inject(Router);
-  const store = inject(Store<{ auth: AuthState }>);
+  const auth = inject(Auth)
 
-  return store.select('auth', 'idToken').pipe(
-    map((idToken) => {
-      console.log(idToken);
-      if (!idToken) {
-        router.navigate(['login']);
-        return false;
+  return new Promise((resolve, reject) => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        resolve(true);
+      } else {
+        router.navigate(['/login']);
+        resolve(false);
       }
-      return true;
-    })
-  )
+    });
+  });
+
 };
