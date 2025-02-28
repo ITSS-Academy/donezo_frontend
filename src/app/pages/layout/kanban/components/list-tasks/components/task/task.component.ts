7e1481b9d132205d089a1a4904caba8fd5f6dd56
HwngLe <hung.lhc04789@sinhvien.hoasen.edu.vn>
2025-02-28 19:29:28 +0700

    feat: Add Task

--- src/app/pages/layout/kanban/components/list-tasks/components/task/task.component.ts
+++ src/app/pages/layout/kanban/components/list-tasks/components/task/task.component.ts
@@ -1,12 +1,33 @@
-import {Component, Input} from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import {
+  MatDialogRef,
+  MAT_DIALOG_DATA, MatDialogTitle, MatDialogContent, MatDialogActions
+} from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-task',
-  standalone: true,
-  imports: [],
   templateUrl: './task.component.html',
-  styleUrl: './task.component.scss'
+  standalone: true,
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, MatDialogTitle, MatDialogContent, MatDialogActions]
 })
 export class TaskComponent {
-  @Input() task: any;
+  taskTitle = '';
+
+  constructor(
+    public dialogRef: MatDialogRef<TaskComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  onSave(): void {
+    this.dialogRef.close(this.taskTitle);
+  }
 }
--- src/app/pages/layout/kanban/components/list-tasks/list-tasks.component.ts
+++ src/app/pages/layout/kanban/components/list-tasks/list-tasks.component.ts
@@ -1,119 +1,73 @@
-import {Component} from '@angular/core';
-import {
-  CdkDrag,
-  CdkDragDrop,
-  CdkDropList,
-  CdkDropListGroup,
-  moveItemInArray,
-  transferArrayItem
-} from '@angular/cdk/drag-drop';
-import {MatButton} from '@angular/material/button';
-import {MaterialModule} from '../../../../../shared/modules/material.module';
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { TaskComponent } from './components/task/task.component';
 
 @Component({
   selector: 'app-list-tasks',
-  standalone: true,
-  imports: [
-    CdkDrag,
-    CdkDropList,
-    CdkDropListGroup,
-    MaterialModule
-  ],
   templateUrl: './list-tasks.component.html',
-  styleUrl: './list-tasks.component.scss'
+  styleUrls: ['./list-tasks.component.scss'],
+  standalone: true,
+  imports: [CommonModule, CdkDropList, CdkDrag, MatButtonModule, MatIconModule]
 })
 export class ListTasksComponent {
-  lists = [
-    {
-      title: "Ideas",
-      tasks: ideas,
+  columns = [
+    { id: 'ideas',
+      title: 'Ideas',
+      tasks: [{ title: 'Task 1', time: '12 Nov 2022' },
+        { title: 'Task 2', time: '13 Nov 2022' },
+        { title: 'Task 7', time: '13 Nov 2022' }]
     },
-    {
-      title: "In Progress",
-      tasks: inProgress,
+    { id: 'inProgress',
+      title: 'In Progress',
+      tasks: [{ title: 'Task 3', time: '14 Nov 2022' },
+        { title: 'Task 4', time: '15 Nov 2022' },
+        { title: 'Task 9', time: '13 Nov 2022' },
+        { title: 'Task 10', time: '13 Nov 2022' }]
     },
-    {
-      title: "Done",
-      tasks: done,
+    { id: 'done',
+      title: 'Done',
+      tasks: [{ title: 'Task 5', time: '16 Nov 2022' },
+        { title: 'Task 6', time: '17 Nov 2022' },
+        { title: 'Task 11', time: '13 Nov 2022' }]
     }
-  ]
+  ];
 
-  drop(event: CdkDragDrop<string[]>) {
+  constructor(public dialog: MatDialog) {}
+
+  get dropListIds() {
+    return this.columns.map(column => column.id);
+  }
+
+  dropColumn(event: CdkDragDrop<any[]>) {
+    moveItemInArray(this.columns, event.previousIndex, event.currentIndex);
+  }
+
+  dropCard(event: CdkDragDrop<any[]>) {
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
-      transferArrayItem(
-        event.previousContainer.data,
-        event.container.data,
-        event.previousIndex,
-        event.currentIndex,
-      );
+      transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
     }
   }
+
+  openDialog(column: any): void {
+    const dialogRef = this.dialog.open(TaskComponent, {
+      width: '300px',
+      data: { column }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        column.tasks.push({ title: result, time: new Date().toLocaleDateString() });
+      }
+    });
+  }
+
+  deleteTask(column: any, task: any): void {
+    column.tasks = column.tasks.filter((t: any) => t !== task);
+  }
 }
-
-
-let ideas = [
-  {
-    title: "Task 1",
-    time: "12 Nov 2022",
-  },
-  {
-    title: "Task 2",
-    time: "12 Nov 2022",
-  },
-  {
-    title: "Task 3",
-    time: "12 Nov 2022",
-  },
-  {
-    title: "Task 4",
-    time: "12 Nov 2022",
-  },
-];
-let inProgress = [
-  {
-    title: "Task 5",
-    time: "12 Nov 2022",
-  },
-  {
-    title: "Task 6",
-    time: "12 Nov 2022",
-  },
-  {
-    title: "Task 7",
-    time: "12 Nov 2022",
-  },
-  {
-    title: "Task 8",
-    time: "12 Nov 2022",
-  },
-  {
-    title: "Task 9",
-    time: "12 Nov 2022",
-  },
-  {
-    title: "Task 10",
-    time: "12 Nov 2022",
-  },
-];
-
-let done = [
-  {
-    title: "Task 11",
-    time: "12 Nov 2022",
-  },
-  {
-    title: "Task 12",
-    time: "12 Nov 2022",
-  },
-  {
-    title: "Task 13",
-    time: "12 Nov 2022",
-  },
-  {
-    title: "Task 14",
-    time: "12 Nov 2022",
-  },
-];
-
